1374c5cb2ef9e3180af4e0566c0206d9896cbab3
glackyy <othmanesticky@gmail.com>
2023-10-23 14:51:59 +0100

    gathering data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.arg[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     completed = [td.get("title") for td in todos
                  if td.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
