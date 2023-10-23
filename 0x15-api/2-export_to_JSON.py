de1edbede10ff670bb681cd4c289f95cd2cb9576
glackyy <othmanesticky@gmail.com>
2023-10-23 15:04:43 +0100

    export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,19 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Returning to-do list information for a given employee id"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+            "task": td.get("title"),
+            "completed": td.get("completed"),
+            "username": username
+        } for td in todos]}, jsonfile)
