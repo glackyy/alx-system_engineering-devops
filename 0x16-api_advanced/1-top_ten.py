ef5de2d1bf9f17b509179e14cdb8a70422c6ebca
glackyy <othmanesticky@gmail.com>
2023-11-07 11:03:29 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,10 +9,10 @@ def top_ten(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    param = {
+    params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, param=param,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
