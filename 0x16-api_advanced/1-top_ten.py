fcf140a21c9f80d781e801baa387c558c110bc97
glackyy <othmanesticky@gmail.com>
2023-11-07 11:00:37 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,21 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Function that print hot posts on a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """Printing the titles of the 10 hot posts on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    param = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, param=param,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    res = response.json().get("data")
+    [print(ch.get("data").get("title")) for ch in res.get("children")]
