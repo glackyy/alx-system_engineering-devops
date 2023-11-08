5aa4352e6392b0eda1763c63b08ed5a5e9efefd6
glackyy <othmanesticky@gmail.com>
2023-11-08 13:22:10 +0100

    recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Function that query a list of all hot posts on a given subreddit"""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returning a list of titles of all hot posts on a given subreddit"""
+    """Returning a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -18,11 +18,13 @@ def recurse(subreddit, hot_list=[], after="", count=0):
                             allow_redirects=False)
     if response.status_code == 404:
         return None
-    res = response.json().get("data")
-    after = res.get("after")
-    count += res.get("dist")
-    for ch in res.get("children"):
-        hot_list.append(ch.get("data").get("title"))
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
     return hot_list
